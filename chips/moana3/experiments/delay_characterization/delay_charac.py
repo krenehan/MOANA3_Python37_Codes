60101a09b2cc01b7fbde26cb9d9be5cc15e4ba37
Kevin Renehan <krenehan@users.noreply.github.com>
2022-06-24 16:48:34 -0400

    Update delay_charac.py

--- chips/moana3/experiments/delay_characterization/delay_charac.py
+++ chips/moana3/experiments/delay_characterization/delay_charac.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 from find_delay import *
 
 # reading from csv data
-
-data1 = pd.read_csv('dword_0to100_rising_sub354.csv', header = None)
-data2 = pd.read_csv('dword_101to200_rising_sub454.csv', header = None)
-data3 = pd.read_csv('dword_201to256_rising_sub554.csv', header = None)
+data1 = pd.read_csv('../../data/delay_characterization/dword_0to100_rising_sub354.csv', header = None)
+data2 = pd.read_csv('../../data/delay_characterization/dword_101to200_rising_sub454.csv', header = None)
+data3 = pd.read_csv('../../data/delay_characterization/dword_201to256_rising_sub554.csv', header = None)
 
 # dropping null value columns to avoid errors
 data1.dropna(inplace = True)
@@ -30,40 +29,46 @@ zero = np.zeros(100, dtype = int)
 arr1_padded = np.empty((101, 350), dtype=int)
 arr2_padded = np.empty((100,350), dtype=int)
 arr3_padded = np.empty((55,350), dtype=int)
+
+# Time axis creation
 t = np.linspace(0,349,num = 350)*65 
 
+# Pad histograms to remove subtractor value change effects
 for histogram in range(len(arr1)):
     arr1_padded[histogram] = np.concatenate((zero, zero,arr1[histogram] ))
     # plt.plot(arr1_padded[histogram])
-
-    
 
 for i in range(len(arr2)):
     arr2_padded[i] = np.concatenate((zero,arr2[i], zero))   
     # plt.plot(arr2_padded[i])
     
-    
 for j in range(len(arr3)):
     arr3_padded[j] = np.concatenate((arr3[j],zero,zero))    
     # plt.plot(arr3_padded[j])
 
+# Concatenate arrays
 arr_combined = np.concatenate((arr1_padded,arr2_padded,arr3_padded))
 
+# Plot raw data
 for h in range(len(arr_combined)):
     plt.plot(arr_combined[h])
     
-delay = find_delay(t,arr_combined[0],arr_combined[1])
+# Find delay steps
 arr_delayed = np.empty((255), dtype = float)
-
 for h in range(len(arr_delayed)):
-    arr_delayed[h] = find_delay(t,arr_combined[h],arr_combined[h+1])
+    arr_delayed[h] = find_delay(t,arr_combined[h],arr_combined[h+1]) * -1
     
-    
-    
+# Plot delay step versus code
+plt.figure()
+plt.plot(arr_delayed)
+plt.title("Delay Step vs Input Code")
+plt.xlabel("Delay Step")
+plt.ylabel("Delay Step (ps)")
 
-
-
-print(delay)
-# # plt.plot(arr1[0])
-# # plt.plot(arr2[0])
-# # plt.plot(arr3[0])
+# Print some stats
+print("Mean Step (ps): " + str(np.mean(arr_delayed)))
+print("Median Step (ps): " + str(np.median(arr_delayed)))
+print("Standard Deviation (ps): " + str(np.std(arr_delayed)))
+print("Minimum Delay Step (ps): " + str(np.amin(arr_delayed)))
+print("Maximum Delay Step (ps): " + str(np.amax(arr_delayed)))
+print("Full Range (ns): " + str(np.sum(arr_delayed)/1000))
