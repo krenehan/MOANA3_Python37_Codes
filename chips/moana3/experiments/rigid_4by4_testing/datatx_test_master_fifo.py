9c9891abc3b6d9eeb066d7f58d93ced03c2f7e6c
jk4537 <jk4537@columbia.edu>
2022-07-13 17:23:39 -0400

    Update datatx_test_master_fifo.py

--- chips/moana3/experiments/rigid_4by4_testing/datatx_test_master_fifo.py
+++ chips/moana3/experiments/rigid_4by4_testing/datatx_test_master_fifo.py
@@ -90,8 +90,8 @@ try:
     # =============================================================================
     # Frame parameters
     # =============================================================================
-    meas_per_patt                   = 32767
-    patt_per_frame                  = 16
+    meas_per_patt                   = 500000
+    patt_per_frame                  = 1
     number_of_frames                = 1
     period                              = round(1/refclk_freq*1e9, 1)
 
@@ -122,49 +122,47 @@ try:
     
     for i in range(number_of_chips):
             
-            # Configure TDC
-            scan_bits[i].TDCStartSelect        = '01111111' if ExternalStart else '1'*8
-            scan_bits[i].TDCStopSelect         = '0'*8 if ExternalStop else '10000000'
-            scan_bits[i].TDCDisable            = '01111111'
-            scan_bits[i].TDCDCBoost            = '01111111' if TDCDCBoost else '1'*8
-            
-            
-            # Configure Pattern Counter
-            scan_bits[i].MeasPerPatt           = np.binary_repr(meas_per_patt, 24)
-            scan_bits[i].MeasCountEnable       = '1'
-            
-            # Configuring Delay Lines
-            dword = 3
-            scan_bits[i].AQCDLLCoarseWord      = np.binary_repr( (dword&0b11110000) >> 4, 4)
-            scan_bits[i].AQCDLLFineWord        = np.binary_repr((dword&0b1110) >> 1, 3)
-            scan_bits[i].AQCDLLFinestWord      = np.binary_repr((dword&0b1), 1)
-            scan_bits[i].DriverDLLWord         = np.binary_repr(5, 5)
-            scan_bits[i].ClkFlip               = '1'
-            scan_bits[i].ClkBypass             = '0'
-            
-            # Configure pattern reset signal
-            scan_bits[i].PattResetControlledByTriggerExt       = '1' if PatternResetWithTriggerExt else '0'
-            scan_bits[i].PattResetExtEnable    = '1' if PatternResetWithExternalSignal else '0'
-            
-            # Configure VCSELs
-            scan_bits[i].VCSELEnableWithScan        = '1' if VCSELEnableExt else '0'
-            scan_bits[i].VCSELEnableControlledByScan        = '1' if VCSELEnableThroughScan else '0'
-            scan_bits[i].VCSELWave1Enable         = '1'
-            scan_bits[i].VCSELWave2Enable         = '1'
-            
-            # Configure TxData
-            scan_bits[i].TestPattEnable        = '0'
-            scan_bits[i].TestDataIn            = np.binary_repr(0, 10)
-            scan_bits[i].TxDataExtRequestEnable = '0'
-            
-            # Configure subtractor
-            scan_bits[i].TimeOffsetWord        = np.binary_repr(454, 10)
-            scan_bits[i].SubtractorBypass      = '0'
-            
-            scan_bits[i].DynamicConfigEnable = '0'
-            
-            # Configure SPADs
-            scan_bits[i].SPADEnable            = '0'*64
+        scan_bits[i].TDCStartSelect        = '0'*8 if ExternalStart else '1'*8
+        scan_bits[i].TDCStopSelect         = '0'*8 if ExternalStop else '1'*8
+        scan_bits[i].TDCDisable            = '11111111'
+        scan_bits[i].TDCDCBoost            = '0'*8 if TDCDCBoost else '1'*8
+        
+        
+        # Configure Pattern Counter
+        scan_bits[i].MeasPerPatt           = np.binary_repr(meas_per_patt, 24)
+        scan_bits[i].MeasCountEnable       = '1'
+        
+        # Configuring Delay Lines
+        scan_bits[i].AQCDLLCoarseWord      = np.binary_repr(0, 4)
+        scan_bits[i].AQCDLLFineWord        = np.binary_repr(0, 3)
+        scan_bits[i].AQCDLLFinestWord      = np.binary_repr(0, 1)
+        scan_bits[i].DriverDLLWord         = np.binary_repr(1, 5)
+        scan_bits[i].ClkFlip               = '0'
+        scan_bits[i].ClkBypass             = '1'
+        
+        # Configure pattern reset signal
+        scan_bits[i].PattResetControlledByTriggerExt       = '1' if PatternResetWithTriggerExt else '0'
+        scan_bits[i].PattResetExtEnable    = '1' if PatternResetWithExternalSignal else '0'
+        
+        # Configure VCSELs
+        scan_bits[i].VCSELEnableWithScan        = '1' if VCSELEnableExt else '0'
+        scan_bits[i].VCSELEnableControlledByScan        = '1' if VCSELEnableThroughScan else '0'
+        scan_bits[i].VCSELWave1Enable         = '1'
+        scan_bits[i].VCSELWave2Enable         = '1'
+        
+        # Configure TxData
+        scan_bits[i].TestPattEnable        = '1'
+        scan_bits[i].TestDataIn            = np.binary_repr(0, 10)
+        scan_bits[i].TxDataExtRequestEnable = '0'
+        
+        # Configure subtractor
+        scan_bits[i].TimeOffsetWord        = np.binary_repr(0, 10)
+        scan_bits[i].SubtractorBypass      = '1'
+        
+        scan_bits[i].DynamicConfigEnable = '0'
+        
+        # Configure SPADs
+        scan_bits[i].SPADEnable            = '0'*64
    
     # Make scan bits for the fpga
     for i in range(number_of_chips):
@@ -180,10 +178,10 @@ try:
     packet = DataPacket.DataPacket(number_of_chips, number_of_frames, patt_per_frame, meas_per_patt, period, compute_mean=False)
         
     # Test pattern in, test pattern out loop
-    for DataIn in range(1, 1000, 8):
+    for DataIn in (0b1111110011,):#range(1, 1023, 8):
         
         # Console header
-        print("------------ Test Pattern Data: {} ------------".format(DataIn))
+        print("------------ Test Pattern Data: " + np.binary_repr(DataIn, 10) + " ------------")
         
         for i in range(number_of_chips):
             
@@ -230,6 +228,7 @@ try:
                 for j in range(300):
                     packet_data = data[frame*bits_in_frame+10*j:frame*bits_in_frame+10*(j+1)]
                     # print("Packet data is " + str(int(packet_data, base=2)) + " and should be " + str(DataIn))
+                    print("Packet data is " + packet_data + " and should be " + str(np.binary_repr(DataIn, 10)))
                     if (int(packet_data, base = 2) != DataIn):
                         test_passed[chip] = False
                         break
