12d35dad5ab9c604a719260fce7652523ae0f1f5
Kevin Renehan <krenehan@users.noreply.github.com>
2023-04-04 13:49:49 -0400

    support for unknown first capture number

--- chips/moana3/data/utilities/zip_files.py
+++ chips/moana3/data/utilities/zip_files.py
@@ -45,14 +45,30 @@ def zip_files():
     
     # Determine the number of captures
     number_of_captures = 0
-    for f in os.listdir():
-        if "capture" in f:
-        # if ".npy" in f:
+    first_capture = -1
+    for f in filelist:
+        if ("capture_" in f) and (".npy" in f):
+            capture_number = int(f.split("capture_")[1].split(".npy")[0])
+            if (first_capture == -1) or (capture_number < first_capture):
+                first_capture = capture_number
+                print(str(first_capture))
             number_of_captures = number_of_captures + 1
             
+    # Check that first capture is not still -1
+    if (number_of_captures <= 0):
+        print(header + "No capture files found")
+        return -1
+    if (first_capture < 0):
+        print(header + "First capture should not be less than 0")
+        return -1
+    
+    # Save first capture number
+    f = open("first_capture.txt", "w")
+    f.write(str(first_capture))
+    f.close()
+            
     # Load the first capture and determine dimensions
-    number_of_chips, number_of_frames, patterns_per_frame, number_of_bins = np.shape(np.load("capture_0.npy"))
-    # number_of_chips, number_of_frames, patterns_per_frame, number_of_bins = np.shape(np.load("1.npy"))
+    number_of_chips, number_of_frames, patterns_per_frame, number_of_bins = np.shape(np.load("capture_" + str(first_capture) + ".npy"))
 
     # Create array
     arr = np.empty( (number_of_captures, number_of_chips, number_of_frames, patterns_per_frame, number_of_bins), dtype=int)   
@@ -61,15 +77,14 @@ def zip_files():
     for capture in range(number_of_captures):
         if not (capture % 10):
             print(header + "Finished loading " + str(capture) + " captures")
-        arr[capture] = np.load("capture_" + str(capture) + ".npy")
-        # arr[capture] = np.load(str(capture) + ".npy")
+        arr[capture] = np.load("capture_" + str(capture+first_capture) + ".npy")
     
     # Zero out the first bin
     for capture in range(number_of_captures):
         for chip in range(number_of_chips):
             for frame in range(number_of_frames):
                 for pattern in range(patterns_per_frame):
-                    arr[capture][chip][frame][pattern][0] = 0.0
+                    arr[capture][chip][frame][pattern][0] = 0
         
     # Zip accumulated results
     print(header + "Saving zipped captures")
